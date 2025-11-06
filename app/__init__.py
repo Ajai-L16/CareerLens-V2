12d88c130976993ca6510c25aa660c03188bcbc4
Ajai-L16 <ajai.l162006@gmail.com>
2025-11-06 20:55:48 +0530

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
-def create_app():
-    """Create and configure an instance of the Flask application."""
-    app = Flask(__name__)
+# Initialize app
+app = Flask(__name__)
 
-    # A secret key is required for session management and other security features
-    # In a real app, this should be loaded from a config file and not hardcoded
-    app.config['SECRET_KEY'] = 'dev'
+# --- CONFIGS ---
+# Set your secret key
+app.config['SECRET_KEY'] = 'a-very-secret-key-that-you-should-change'
 
-    # Register the routes
-    from . import routes
-    app.register_blueprint(routes.bp)
+# Set your database URI
+# FORMAT: mysql+<driver>://<user>:<password>@<host>/<dbname>
+# We are using 'mysqlclient' as the driver
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlclient://root:Apassword@localhost/careerlensdb'
 
-    return app
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# --- INITIALIZE EXTENSIONS ---
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# This tells Flask-Login which route to redirect to if a user
+# tries to access a page that requires them to be logged in.
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info' # For flashing messages
+
+# --- Import routes and models ---
+# This is done *after* 'app' and 'db' are created to avoid circular imports.
+from app import routes, models
